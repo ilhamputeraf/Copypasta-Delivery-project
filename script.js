8a04576b8b90ddff256fd37174f011b20c6e1239
Putera <ilhamputeraf30@gmail.com>
2024-11-14 19:20:46 +0700

    changes

--- script.js
+++ script.js
@@ -16,3 +16,28 @@ menuItems.forEach(item => {
         mobileMenu.classList.add('hidden'); 
     });
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const themeToggleButton = document.getElementById('theme-toggle');
+    const body = document.body;
+
+    const currentTheme = localStorage.getItem('theme');
+    if (currentTheme) {
+        body.classList.add(currentTheme);
+    }
+
+    themeToggleButton.addEventListener('click', () => {
+        body.classList.toggle('dark');
+        
+        if (body.classList.contains('dark')) {
+            localStorage.setItem('theme', 'dark');
+            themeToggleButton.textContent = '🌞';
+        } else {
+            localStorage.removeItem('theme');
+            themeToggleButton.textContent = '🌙';
+        }
+    });
+});
+
+
+
